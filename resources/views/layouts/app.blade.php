3b7075d27f46462c547252763ed20f662d24c7b4
2020Abdullah <abdallamohmad999@gmail.com>
2025-07-30 17:53:01 +0300

    fix bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-        <title>{{ config('app.name', 'شركة الفتح') }}</title>
+        <title>{{ config('app.name') }}</title>
         <link rel="apple-touch-icon" href="{{ asset('assets/images/web/logo.ico') }}">
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/web/icon.png') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/css/vendors-rtl.min.css') }}">
